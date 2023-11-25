bcd519be04f3a9cad035fc6c1800d444da865b41
Uvaish7007 <146409964+Uvaish7007@users.noreply.github.com>
2023-11-25 10:58:00 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecommerce</title>
+    <!-- CDN PASTE HERE -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+     <!-- FONT AWESOME HERE -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+
+<body>
+<nav class="navbar navbar-light bg-light>
+  <div class="container-fluid font-monospace ">
+  <a class="navbar-brand text-warning" > <h1>Shopsy Cart</h1></a>
+
+  <div class="d-flex">
+ <a href="" class="text-warning text-decoration-none pe-3"><i class="fa-solid fa-house"></i>Home</a>
+ <a href="" class="text-warning text-decoration-none pe-3"> <i class="fa-solid fa-cart-shopping"></i>Cart(0)</a>
+
+
+ <span class="text-warning"> <i class="fas fa-user-shield"></i> Hello,
+<a href="" class="text-warning text-decoration-none pe-3"> <i class="fa-solid fa-right-to-bracket"></i>Login</a>
+<a href="../admin/mystore.php" class="text-warning text-decoration-none pe-3"> <i class="fa-solid fa-lock"></i>Admin</a>
+
+
+ </span>
+
+</nav>
+  </div>
+
+  <div class="bg-warning ">
+    <ul  class="list-unstyled d-flex  justify-content-center ">
+      <li><a href=""  class="text-white text-decoration-none fs-4 px-5">LAPTOPS</a></li>
+      <li><a href=""  class="text-white text-decoration-none fs-4 px-5">MOBILES</a></li>
+      <li><a href=""  class="text-white text-decoration-none fs-4 px-5">BAGS</a></li>
+    </ul>
+  </div>
+
+</body>
+</html>
