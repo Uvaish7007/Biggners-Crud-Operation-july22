7d4502d5e336a1e49644e9ea714cff1ae0ac3c70
Uvaish7007 <146409964+Uvaish7007@users.noreply.github.com>
2023-11-25 10:52:29 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+
+$con= new mysqli('localhost','root','','data');
+
+// if($con){
+//  echo("connection succesfully");
+// }
+
+// else
+// {
+//     die("mysqli_error"($con)); 
+// }
+
+// ?>
